ce069c69555a64ed30e22e4a085fa442a3c47e1e
talfig <talfig8@gmail.com>
2024-08-15 15:19:57 +0300

    Removed Macros
    I removed the macro defined in the header file.

--- Header Files/token_utils.h
+++ Header Files/token_utils.h
@@ -1,9 +1,6 @@
 #ifndef TOKEN_UTILS_H
 #define TOKEN_UTILS_H
 
-#define MIN_CHAR_VALUE 32 /* The first printable ASCII character */
-#define MAX_CHAR_VALUE 126 /* The last printable ASCII character */
-
 int nextToken(char *dest, char **ptr, const char delim);
 
 int nextString(char *dest, char **ptr, int line_counter);
